c6217c689a45df48d4ac3483d736ed87305a45ee
martin <martinrevert@gmail.com>
2018-03-14 10:37:37 -0300

    - Clean unused intent of parse hexa code to int and passing to binding.

--- app/src/main/java/com/teamwork/teamworktestapp/viewModel/ProjectsItemViewModel.java
+++ app/src/main/java/com/teamwork/teamworktestapp/viewModel/ProjectsItemViewModel.java
@@ -22,9 +22,6 @@ public class ProjectsItemViewModel extends BaseObservable {
     private Context mContext;
     private Project mProject;
 
-    @Bindable
-    private int color;
-
     public ProjectsItemViewModel(Context context, Project project) {
         this.mContext = context;
         this.mProject = project;
@@ -46,24 +43,6 @@ public class ProjectsItemViewModel extends BaseObservable {
     public Boolean getStarred() {
         return mProject.getStarred();
     }
-
-/*
-Hmmm...Android Databinding cannot manage hexadecimal colors directly, only manages specific
-int resources or drawables, so you cannot work with dynamic hexa colors without doing some nasty conversion.
-
-https://stackoverflow.com/questions/32992211/use-databinding-library-to-set-background-color-resource-or-null/33065020
-
-    public int getColors() {
-        String colorstring = mProject.getCategory().getColor();
-
-        if (colorstring.equals("")){
-            color = Color.parseColor("#000000");
-        }else{
-            color = Color.parseColor(colorstring);
-        }
-        Log.v("Color", String.valueOf(color));
-        return color;
-    } */
 
     public String getStatus() {
         return mProject.getStatus();

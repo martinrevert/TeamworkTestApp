0bbe5f9514238e6b2536ce725e7a9d7faad4f8f2
martin <martinrevert@gmail.com>
2018-03-14 10:38:45 -0300

    - Remove unused imports

--- app/src/main/java/com/teamwork/teamworktestapp/di/components/ApplicationComponent.java
+++ app/src/main/java/com/teamwork/teamworktestapp/di/components/ApplicationComponent.java
@@ -4,11 +4,9 @@ package com.teamwork.teamworktestapp.di.components;
  * Created by martin on 06/03/18.
  */
 
-import android.app.Application;
-
 import com.teamwork.teamworktestapp.data.DataManager;
 import com.teamwork.teamworktestapp.di.modules.ApplicationModule;
-import com.teamwork.teamworktestapp.view.activity.MainActivity;
+
 
 import javax.inject.Singleton;
 
